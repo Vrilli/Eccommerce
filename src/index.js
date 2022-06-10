4f8b04d19821e91f917d9a3bb24547e843b8f744
Lizeth Paola Delgadillo <lizdelga001@gmail.com>
2022-06-10 13:02:03 -0500

    Cambios sin errores

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './redux/store/store';
-import AppRouters from './routers/AppRouters';
-import Pagos from './components/Pagos';
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Pagos />
-  </Provider>,
-=======
 import Home from './components/Home';
 import { CartProvider } from './Context/CartContext';
 
@@ -21,7 +9,6 @@ ReactDOM.render(
   <CartProvider>
     <Home />
   </CartProvider>,
->>>>>>> develop
   document.getElementById('root')
 );
 
